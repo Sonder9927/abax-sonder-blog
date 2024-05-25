79ea558041d9ac8753e1ed28d38c7fba59c02706
sonder <merak19981999@gmail.com>
2024-05-25 17:27:07 +0800

    info about me

--- src/config.ts
+++ src/config.ts
@@ -17,10 +17,10 @@ export const NAV_ITEMS: NavItems = {
 		path: "/projects",
 		title: "projects",
 	},
-	media: {
-		path: "/media",
-		title: "media",
-	},
+	// media: {
+	// 	path: "/media",
+	// 	title: "media",
+	// },
 	about: {
 		path: "/about",
 		title: "about",
@@ -29,26 +29,26 @@ export const NAV_ITEMS: NavItems = {
 
 export const SITE = {
 	// Your site's detail?
-	name: "Ink",
-	title: "Astro - Ink",
-	description: "Crisp, minimal, personal blog theme for Astro",
-	url: "https://astro-ink.vercel.app",
-	githubUrl: "https://github.com/one-aalam/astro-ink",
+	name: "Sonder Merak",
+	title: "Abax - Sonder",
+	description: "Youth sentiment is always poetry.",
+	url: "https://abax-sonder.vercel.app/",
+	githubUrl: "https://github.com/sonder9927/abax-sonder-blog",
 	listDrafts: true,
 	image:
-		"https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
+		"https://photos.fife.usercontent.google.com/pw/AP1GczPqSaSpZCZCILTCBKtjrAYXv2yNc5_iUgZ4oOwkNJOJDh187_3qHU4=w1155-h722-s-no-gm?authuser=0",
 	// YT video channel Id (used in media.astro)
 	ytChannelId: "",
 	// Optional, user/author settings (example)
 	// Author: name
-	author: "", // Example: Fred K. Schott
+	author: "Sonder Merak", // Example: Fred K. Schott
 	// Author: Twitter handler
 	authorTwitter: "", // Example: FredKSchott
 	// Author: Image external source
 	authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
 	// Author: Bio
 	authorBio:
-		"Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
+		"Youth sentiment is always poetry.",
 };
 
 // Ink - Theme configuration
---
+++ src/pages/_abax-sonder.md
@@ -0,0 +1,5 @@
+**Abax** is an invisible force. Following the natural laws without slacking off, the gathering and dispersing is like life and death without any trace ...
+
+### Per Aspera Ad Astra!
+**Maintained By:** [@Sonder Merak](https://github.com/sonder9927)
+                
